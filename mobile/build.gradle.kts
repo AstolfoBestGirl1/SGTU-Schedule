49a24ee0d365160a7494c2ca61a45b2a93458e52
AstolfoBestGirl <astolfobestgirl1+github@proton.me>
2026-09-16 07:11:27 +0400

    v1.0.0 another building fixes 3

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -121,7 +121,6 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.play.services.wearable)
     testImplementation(libs.junit)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
