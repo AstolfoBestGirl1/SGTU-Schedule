2510a2743f08f395a152e8d1faaaf9748116a152
AstolfoBestGirl <astolfobestgirl1+github@proton.me>
2026-09-16 06:39:31 +0400

    v1.0.0 building fix

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -40,7 +40,7 @@ android {
             if (keystorePropertiesFile.exists()) {
                 val keystoreProperties = Properties()
                 keystoreProperties.load(FileInputStream(keystorePropertiesFile))
-                storeFile = file(keystoreProperties["storeFile"] as String)
+                storeFile = rootProject.file(keystoreProperties["storeFile"] as String)
                 storePassword = keystoreProperties["storePassword"] as String
                 keyAlias = keystoreProperties["keyAlias"] as String
                 keyPassword = keystoreProperties["keyPassword"] as String
